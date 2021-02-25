2ee5725898d0a04c8fa18eabc9fa1b551d2cf626
Dmitry Samoylenko <dimsamoylenko@gmail.com>
2021-02-25 22:58:50 +0300

    made guess number game

--- mybot/bot.py
+++ mybot/bot.py
@@ -1,4 +1,5 @@
 import logging
+from random import randint
 
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
@@ -18,6 +19,7 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("guess", guess_number))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info("Бот стартовал")
@@ -37,6 +39,30 @@ def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
     update.message.reply_text(user_text)
+
+
+def guess_number(update, context):
+    print(context.args)
+    if context.args:
+        try:
+            user_number = int(context.args[0])
+            message = play_random_numbers(user_number)
+        except (TypeError, ValueError):
+            message = "Введите целое число"
+    else:
+        message = "Введите число"
+    update.message.reply_text(message)
+
+
+def play_random_numbers(user_number):
+    bot_number = randint(user_number - 10, user_number + 10)
+    if user_number > bot_number:
+        message = f"Ваше число {user_number}, мое {bot_number}. Вы выиграли."
+    elif user_number == bot_number:
+        message = f"Ваше число {user_number}, мое {bot_number}. Ничья."
+    else:
+        message = f"Ваше число {user_number}, мое {bot_number}. Вы проиграли."
+    return message
 
 
 main()
