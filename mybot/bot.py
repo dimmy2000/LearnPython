4eee0706c3222878c754747d1505aec2c5b43ff8
Dmitry Samoylenko <dimsamoylenko@gmail.com>
2021-02-26 19:31:53 +0300

    added emojis & some doc strings

--- mybot/bot.py
+++ mybot/bot.py
@@ -1,10 +1,13 @@
 from glob import glob
+from emoji import emojize
 import logging
 from random import randint, choice
-from settings import API_KEY, PROXY_URL, PROXY_USERNAME, PROXY_PASSWORD
+
+from settings import API_KEY, PROXY_URL, PROXY_USERNAME, PROXY_PASSWORD, USER_EMOJI
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
-logging.basicConfig(filename='bot.log', level=logging.INFO)
+logging.basicConfig(filename='bot.log', format='%(asctime)s %(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
 
 # Настройки прокси
 PROXY = {'proxy_url': PROXY_URL,
@@ -31,21 +34,30 @@ def main():
 
 
 def greet_user(update, context):
+    # функция для приветствия пользователя
     print('Вызван /start')
     chat_id = update.effective_chat.id
     file_id = "AgACAgIAAxkBAAEIYOJgOBL3LbcH5fVOAAG0BN36mKvs-2sAAmGyMRuOBcBJMVNMndUQ2S1U9hibLgADAQADAgADbQADoacCAAEeBA"
     context.bot.send_photo(chat_id=chat_id, photo=file_id)
-    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start\n'
+    context.user_data['emoji'] = get_smile(context.user_data)
+    update.message.reply_text(f'Привет, пользователь! {context.user_data["emoji"]} Ты вызвал команду /start\n'
                               f'Ну и {update.message.from_user.first_name} же ты. Восхитительно!')
 
 
 def talk_to_me(update, context):
+    # функция эхо - повторяем сообщения пользователя
+    context.user_data['emoji'] = get_smile(context.user_data)
+    username = update.effective_user.first_name
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(user_text)
+    update.message.reply_text(f'Отличная работа, {username} {context.user_data["emoji"]}! Ты написал: {user_text}')
 
 
 def guess_number(update, context):
+    '''
+    Игра "Угадай число": пользователь выбирает число, бот выбирает случайное число - победа достается тому, у кого
+    больше величина.
+    '''
     print(context.args)
     if context.args:
         try:
@@ -59,6 +71,7 @@ def guess_number(update, context):
 
 
 def play_random_numbers(user_number):
+    # функция для игры в "угадай число"
     bot_number = randint(user_number - 10, user_number + 10)
     if user_number > bot_number:
         message = f"Ваше число {user_number}, мое {bot_number}. Вы выиграли."
@@ -70,10 +83,19 @@ def play_random_numbers(user_number):
 
 
 def send_cat_picture(update, context):
+    # функция передающая по запросу случайную картинку с котиком из папки на компьютере
     cat_photos_list = glob('images/cat*.jp*g')
     cat_pic_filename = choice(cat_photos_list)
     chat_id = update.effective_chat.id
     context.bot.send_photo(chat_id=chat_id, photo=open(cat_pic_filename, 'rb'))
 
+def get_smile(user_data):
+    # функция, которая возвращает случайный смайлик из списка
+    if 'emoji' not in user_data:
+        smile = choice(USER_EMOJI)
+        return emojize(smile, use_aliases=True)
+    return user_data['emoji']
 
-main()
+
+if __name__ == "__main__":
+    main()
