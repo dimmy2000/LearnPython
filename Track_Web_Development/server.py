695c7b41a7d08a2fcbec41d9f6b8eadc822bea82
Dmitry Samoylenko <dimsamoylenko@gmail.com>
2021-03-02 18:56:48 +0300

    refactor code, reformat index.html

--- Track_Web_Development/server.py
+++ Track_Web_Development/server.py
@@ -1,16 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    title = 'Новости Python'
     weather = weather_by_city("Moscow,Russia")
-    if weather:
-        return f'Погода: {weather["temp_C"]}ºC, {weather["lang_ru"][0]["value"].lower()}. ' \
-               f'Ощущается как {weather["FeelsLikeC"]}ºC'
-    else:
-        return "Сервис погоды временно недоступен."
+    return render_template('index.html', page_title=title, weather=weather)
 
 if __name__ == "__main__":
     app.run(debug=True)
