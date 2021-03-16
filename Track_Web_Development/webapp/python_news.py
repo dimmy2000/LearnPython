4536867b8fca1e2e5100f9c4135abc8e4a6993fd
Dmitry Samoylenko <dimsamoylenko@gmail.com>
2021-03-16 15:12:40 +0300

    added news export from db to index.html

--- Track_Web_Development/webapp/python_news.py
+++ Track_Web_Development/webapp/python_news.py
@@ -1,7 +1,11 @@
 import requests
 
 from bs4 import BeautifulSoup
+from datetime import datetime
 from flask import current_app
+
+from webapp.model import db, News
+
 
 def get_html(url):
     try:
@@ -18,15 +22,21 @@ def get_python_news():
     if html:
         soup = BeautifulSoup(html, 'html.parser')
         all_news = soup.find('ul', class_='list-recent-posts').find_all('li')
-        result_news = []
         for news in all_news:
             title = news.find('a').text
             url = news.find('a')['href']
-            published = news.find('time').text
-            result_news.append({
-                "title": title,
-                "url": url,
-                "published": published
-            })
-        return result_news
-    return False
+            published = news.find('time')['datetime']
+            try:
+                published = datetime.strptime(published, '%Y-%m-%d')
+            except(ValueError):
+                published = datetime.now()
+            save_news(title, url, published)
+
+
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)
+    if not news_exists:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
