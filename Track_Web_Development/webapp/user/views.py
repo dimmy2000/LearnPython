14a479ea25c3a922b3ad9ddacbd1be20745e1a73
dimmy2000 <dimsamoylenko@gmail.com>
2021-04-05 13:03:10 +0300

    added navbar

--- Track_Web_Development/webapp/user/views.py
+++ Track_Web_Development/webapp/user/views.py
@@ -10,10 +10,10 @@ blueprint = Blueprint('user', __name__, url_prefix='/users')
 @blueprint.route('/login')
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('news.index'))
     title = 'Авторизация'
     login_form = LoginForm()
-    return render_template('login.html', page_title=title,
+    return render_template('user/login.html', page_title=title,
                            form=login_form)
 
 
@@ -22,11 +22,11 @@ def process_login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter(username=form.username.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             flash('Вы вошли на сайт')
-            return redirect(url_for('index'))
+            return redirect(url_for('news.index'))
 
     flash('Неправильное имя пользователя или пароль')
     return redirect(url_for('login'))
@@ -36,4 +36,4 @@ def process_login():
 def logout():
     logout_user()
     flash('Вы успешно разлогинились')
-    return redirect(url_for('index'))
+    return redirect(url_for('news.index'))
