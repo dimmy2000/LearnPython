f1162201101212e142dd36d2842d83b11206d0c3
dimmy2000 <dimsamoylenko@gmail.com>
2021-04-05 22:40:26 +0300

    added news parser for habr.com

---
+++ Track_Web_Development/webapp/news/parsers/utils.py
@@ -0,0 +1,26 @@
+import requests
+
+from webapp.db import db
+from webapp.news.models import News
+
+
+def get_html(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
+    }
+    try:
+        result = requests.get(url, headers=headers)
+        result.raise_for_status()
+        return result.text
+    except(requests.RequestException, ValueError):
+        print("Сетевая ошибка")
+        return False
+
+
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)
+    if not news_exists:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
