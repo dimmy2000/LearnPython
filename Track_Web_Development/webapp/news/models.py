3d799f493dd374c115312a45d608f1d029ad46bb
dimmy2000 <dimsamoylenko@gmail.com>
2021-05-17 15:35:22 +0300

    added news comment functionality

--- Track_Web_Development/webapp/news/models.py
+++ Track_Web_Development/webapp/news/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from sqlalchemy.orm import relationship
+
 from webapp.db import db
 
 
@@ -9,6 +11,9 @@ class News(db.Model):
     url = db.Column(db.String, unique=True, nullable=False)
     published = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.Text, nullable=True)
+
+    def comments_count(self):
+        return Comment.query.filter(Comment.news_id == self.id).count()
 
     def __repr__(self):
         return '<News {} {}>'.format(self.title, self.url)
@@ -28,6 +33,8 @@ class Comment(db.Model):
         db.ForeignKey('user.id', ondelete='CASCADE'),
         index=True,
     )
+    news = relationship('News', backref='comments')
+    user = relationship('User', backref='comments')
 
     def __repr__(self):
         return '<Comment {}>'.format(self.id)
--- Track_Web_Development/webapp/news/views.py
+++ Track_Web_Development/webapp/news/views.py
@@ -1,6 +1,11 @@
-from flask import Blueprint, abort, current_app, render_template
+from flask import Blueprint, abort, current_app, flash, redirect, \
+    render_template, request
+from flask_login import current_user, login_required
 
-from webapp.news.models import News
+from webapp.db import db
+from webapp.news.forms import CommentForm
+from webapp.news.models import Comment, News
+from webapp.utils import get_redirect_target
 from webapp.weather import weather_by_city
 
 blueprint = Blueprint('news', __name__)
@@ -10,7 +15,8 @@ blueprint = Blueprint('news', __name__)
 def index():
     title = 'Новости Python'
     weather = weather_by_city(current_app.config["WEATHER_DEFAULT_CITY"])
-    news_list = News.query.filter(News.text.isnot(None)).order_by(News.published.desc()).all()
+    news_list = News.query.filter(News.text.isnot(None)).order_by(
+        News.published.desc()).all()
     return render_template('news/index.html', page_title=title,
                            weather=weather, news_list=news_list)
 
@@ -18,8 +24,30 @@ def index():
 @blueprint.route('/news/<int:news_id>')
 def single_news(news_id):
     my_news = News.query.filter(News.id == news_id).first()
+
     if not my_news:
         abort(404)
-    return render_template('news/single_news.html',
-                           page_title=my_news.title,
-                           news=my_news)
+    comment_form = CommentForm(news_id=my_news.id)
+    return render_template('news/single_news.html', page_title=my_news.title,
+                           news=my_news, comment_form=comment_form)
+
+
+@blueprint.route('/news/comment/', methods=['POST'])
+@login_required
+def add_comment():
+    comment_form = CommentForm()
+    if comment_form.validate_on_submit():
+        comment = Comment(text=comment_form.comment_text.data,
+                          news_id=comment_form.news_id.data,
+                          user_id=current_user.id)
+        db.session.add(comment)
+        db.session.commit()
+        flash('Комментарий успешно добавлен')
+    else:
+        for field, errors in comment_form.errors.items():
+            for error in errors:
+                flash('Ошибка в заполнении поля "{}": - {}'.format(
+                    getattr(comment_form, field).label.text,
+                    error,
+                ))
+    return redirect(get_redirect_target())
